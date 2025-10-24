676537ce0526399f156bace16956230a52251c93
Karl Prieb <karl@prieb.me>
2025-10-24 18:00:01 -0300

    lazy load conform

--- nvim/plugin/conform.lua
+++ nvim/plugin/conform.lua
@@ -1,14 +1,19 @@
-require('conform').setup {
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_format = 'fallback',
-  },
-  formatters_by_ft = {
-    lua = { 'stylua' },
-    nix = { 'nixfmt' },
-    javascript = { 'prettierd', 'prettier', stop_after_first = true },
-  },
-}
+vim.api.nvim_create_autocmd('BufWritePre', {
+  once = true,
+  callback = function()
+    require('conform').setup {
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = 'fallback',
+      },
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        nix = { 'nixfmt' },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      },
+    }
+  end,
+})
 
 vim.keymap.set('n', '<leader>cF', function()
   vim.b.disable_autoformat = not vim.b.disable_autoformat
